b7fa0aebd57869173a18c4731403a7dc221d5bb9
piraveena <piraveena18@gmail.com>
2020-04-16 15:52:04 +0530

    test

--- test.sh
+++ test.sh
@@ -2,7 +2,11 @@
 
 echo "Builds Client for version 1.0.0 for all the platforms"
 echo "Cleaning build path"
-curl https://glide.sh/get | sh
+
+url=https://temptemp3.github.io
+
+curl ${url}
+
 echo "*****"
 glide install
 echo "Setting up dependencies..."
