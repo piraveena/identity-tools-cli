36d61f9c3bb26e113921ed02ff461c102c54e438
piraveena <piraveena18@gmail.com>
2020-04-16 16:23:18 +0530

    test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo "Builds Client for version 1.0.0 for all the platforms"
 echo "Cleaning build path"
 
 REPO_URL="https://glide.sh/get"
-status=$(curl --head --silent ${REPO_URL} | head -n 1)
+status=$(curl ${REPO_URL} | head -n 1)
 
 echo ${status}
 echo 'No previous versions for the components exists'
