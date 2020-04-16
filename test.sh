c93dee198d359aa8ccffc1ac03acc7123926d7da
piraveena <piraveena18@gmail.com>
2020-04-16 15:43:37 +0530

    test

--- test.sh
+++ test.sh
@@ -2,10 +2,8 @@
 
 echo "Builds Client for version 1.0.0 for all the platforms"
 
-ENV GOPATH /build/software/go/go
-ENV PATH $GOPATH/bin:/build/software/go/go-1.12.5/bin:$PATH
-RUN mkdir -p "$GOPATH/src/github.com/wso2" "$GOPATH/bin" && chmod -R 777 "$GOPATH"
-RUN curl https://glide.sh/get | sh
+
+curl https://glide.sh/get | sh
 
 echo "Cleaning build path"
 
