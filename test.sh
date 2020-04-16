51019743c68fc816804d5b23f485110f8438a621
piraveena <piraveena18@gmail.com>
2020-04-16 15:50:13 +0530

    test

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@
 echo "Builds Client for version 1.0.0 for all the platforms"
 echo "Cleaning build path"
 curl https://glide.sh/get | sh
+echo "*****"
 glide install
 echo "Setting up dependencies..."
